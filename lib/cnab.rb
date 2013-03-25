bbb4ed3da5d6e758f644ee8b0bb2ed0af9e4ba12
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-03-25 14:09:46 -0300

    Remodeling classes

--- lib/cnab.rb
+++ lib/cnab.rb
@@ -3,4 +3,7 @@ require "cnab/version"
 module Cnab
   autoload :HeaderArquivo, 'cnab/header_arquivo'
   autoload :HeaderLote, 'cnab/header_lote'
+  autoload :TrailerArquivo, 'cnab/trailer_arquivo'
+  autoload :TrailerLote, 'cnab/trailer_lote'
+  autoload :Detalhe, 'cnab/detalhe'
 end
---
+++ lib/cnab/detalhe.rb
@@ -0,0 +1,6 @@
+module Cnab
+  module Detalhe
+    autoload :SegmentoT, 'cnab/detalhe/segmento_t'
+    autoload :SegmentoU, 'cnab/detalhe/segmento_u'
+  end
+end

b6e3dfd2d8c5cd216d5317ce36fd866277b7aea5
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-03-27 13:56:53 -0300

    Added a better #inspect output [ci skip]

--- lib/cnab/helper.rb
+++ lib/cnab/helper.rb
@@ -3,5 +3,12 @@ module Cnab
     def method_missing(method)
       instance_variable_get("@#{method}")
     end
+
+    def inspect
+      variables = instance_variables.map do |instance_variable|
+        "#{instance_variable.to_s.gsub('@', '')}: \"#{instance_variable_get(instance_variable)}\""
+      end.join(", ")
+      "<#{self.class.name} #{variables}>"
+    end
   end
 end
